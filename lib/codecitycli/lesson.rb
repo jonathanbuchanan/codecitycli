e2dc3e3160bf6b78537a8751bbd0fe4feb9181c4
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-11-09 22:23:09 -0500

    Added a lesson model

---
+++ lib/codecitycli/lesson.rb
@@ -0,0 +1,11 @@
+module CodeCityCLI
+  class Exercise
+    attr_accessor :title
+    attr_accessor :id
+    attr_accessor :value
+    attr_accessor :slides
+
+    def initialize
+    end
+  end
+end
