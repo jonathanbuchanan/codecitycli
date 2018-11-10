8cc50aee0c20c98a221920d5b3191bc4f932a460
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-11-09 22:25:22 -0500

    Added an exercise model

---
+++ lib/codecitycli/exercise.rb
@@ -0,0 +1,12 @@
+module CodeCityCLI
+  class Exercise
+    attr_accessor :id
+    attr_accessor :title
+    attr_accessor :instructions
+    attr_accessor :test
+    attr_accessor :value
+
+    def initialize
+    end
+  end
+end
