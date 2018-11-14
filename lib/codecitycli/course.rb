dc14e37a4e4dc8919a176e287db8e62d559f3960
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-11-14 13:24:21 -0500

    Added 'fetch' and 'submit' as subcommands for course

--- lib/codecitycli/course.rb
+++ lib/codecitycli/course.rb
@@ -1,4 +1,6 @@
 require 'thor'
+require 'codecitycli/lesson'
+require 'codecitycli/exercise'
 
 module CodeCityCLI
   class Course
@@ -69,10 +71,20 @@ module CodeCityCLI
       end
 
       desc 'list', 'lists courses'
+      option :level, type: :string, default: 'course'
+      option :course_id, type: :numeric
+      option :lesson_id, type: :numeric
       def list
-        courses = CodeCityCLI::Course.index
-        courses.each do |course|
-          print("ID: #{course.id}, Title: #{course.title}\n")
+        case options[:level]
+        when 'course'
+          list_courses
+        when 'lesson'
+          list_lessons(options[:course_id])
+        when 'exercise'
+          list_exercises(options[:course_id], options[:lesson_id])
+        else
+          # Error!
+          puts('level must be course, lesson, or exercise')
         end
       end
 
@@ -94,6 +106,44 @@ module CodeCityCLI
         course = CodeCityCLI::Course.new(id: course_id)
 
         course.disenroll CodeCityCLI::User.current_user
+      end
+
+      desc 'submit FILE', 'submits FILE as a solution to the exercise'
+      option :exercise_id, type: :numeric, required: true
+      def submit(file)
+        exercise = CodeCityCLI::Exercise.new(id: options[:exercise_id])
+
+        # exercise.submit
+      end
+
+      desc 'fetch EXERCISE_ID', 'fetches the exercise with EXERCISE_ID'
+      def fetch(exercise_id)
+        exercise = CodeCityCLI::Exercise.new(id: options[:exercise_id])
+
+        # exercise.get
+      end
+
+      private
+      def list_courses
+        courses = CodeCityCLI::Course.index
+        courses.each do |course|
+          print("Course | ID: #{course.id}, Title: #{course.title}\n")
+        end
+
+      end
+
+      def list_lessons(course_id)
+        lessons = CodeCityCLI::Lesson.index(course_id)
+        lessons.each do |lesson|
+          print("Lesson | ID: #{lesson.id}, Title: #{lesson.title}\n")
+        end
+      end
+
+      def list_exercises(course_id, lesson_id)
+        exercises = CodeCityCLI::Exercise.index(course_id, lesson_id)
+        exercises.each do |exercises|
+          print("Exercise | ID: #{exercise.id}, Title: #{exercise.title}\n")
+        end
       end
     end
   end
