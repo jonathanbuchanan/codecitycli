db4e881276ee754ea6f83780be66e8b11b147e6b
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2019-02-06 11:59:22 -0500

    Started testing exercises

--- lib/codecitycli/exercise.rb
+++ lib/codecitycli/exercise.rb
@@ -1,42 +1,20 @@
 module CodeCityCLI
   class Exercise
-    attr_accessor :title
-    attr_accessor :id
-    attr_accessor :lesson_id
-    attr_accessor :course_id
-    attr_accessor :value
-    attr_accessor :slides
+    attr_accessor :test_path
+    attr_accessor :test_name
+    attr_accessor :exercise_path
+    attr_accessor :exercise_name
 
     def initialize(args)
-      self.title = args[:title] if args[:title]
-      self.id = args[:id] if args[:id]
-      self.lesson_id = args[:lesson_id] if args[:lesson_id]
-      self.course_id = args[:course_id] if args[:course_id]
-      self.value = args[:value] if args[:value]
-      self.slides = args[:slides] if args[:slides]
-    end
-
-    def show(user)
-      response = Request.get("/courses/#{self.course_id}/lessons/#{self.lesson_id}/exercises/#{self.id}", {}, user.account.token.headers)[:body]
-
-      test_file = get_file(response[:test_path])
-      exercise_file = get_file(response[:exercise_path])
-
-      test_path = Config.instance.directory + '/' + course_name + '/' + lesson_name + '/' + response[:test_name]
-      exercise_path = Config.instance.directory + '/' + course_name + '/' + lesson_name + '/' + response[:exercise_name]
-
-      save_file(test_path, test_file)
-      save_file(exercise_path, exercise_file)
-
-      return
-    end
-
-    def fetch(user)
-      response = Request.get("/courses/#{self.course_id}/lessons/#{self.lesson_id}/exercises/#{self.id}", {}, user.account.token.headers)[:body]
+      self.test_path = args[:test_path] if args[:test_path]
+      self.test_name = args[:test_name] if args[:test_name]
+      self.exercise_path = args[:exercise_path] if args[:exercise_path]
+      self.exercise_name = args[:exercise_name] if args[:exercise_name]
     end
 
     def push(submission_path, user)
-      submission = get_local_file(submission_path)
+      #submission = get_local_file(submission_path)
+      submission = ""
 
       response = Request.post("/courses/#{self.course_id}/lessons/#{self.lesson_id}/exercises/push", { id: self.id, submission: submission }, user.account.token.headers)
       response[:body]
@@ -46,9 +24,41 @@ module CodeCityCLI
       # Run the test
     end
 
-    def self.index(user, course_id)
-      # GET /courses/course_id/lessons/
-      # return exercises
+    def self.fetch(course_id, lesson_id, exercise_id, user)
+      response = Request.get("/courses/#{course_id}/lessons/#{lesson_id}/exercises/#{exercise_id}", {}, user.account.token.headers)
+
+      unless response != nil
+        raise APIError, "got no response"
+      end
+      unless response[:body]
+        raise APIError, "response has no body"
+      end
+      unless response[:body][:test_path]
+        raise APIError, "response has no test path"
+      end
+      unless response[:body][:test_name]
+        raise APIError, "response has no test name"
+      end
+      unless response[:body][:exercise_path]
+        raise APIError, "response has no exercise path"
+      end
+      unless response[:body][:exercise_name]
+        raise APIError, "response has no exercise name"
+      end
+
+      return self.new(test_path: response[:body][:test_path],
+        test_name: response[:body][:test_name],
+        exercise_path: response[:body][:exercise_path],
+        exercise_name: response[:body][:exercise_name])
+
+      #test_file = get_file(response[:body][:test_path])
+      #exercise_file = get_file(response[:body][:exercise_path])
+
+      #test_path = Config.instance.directory + '/' + course_name + '/' + lesson_name + '/' + response[:body][:test_name]
+      #exercise_path = Config.instance.directory + '/' + course_name + '/' + lesson_name + '/' + response[:body][:exercise_name]
+
+      #save_file(test_path, test_file)
+      #save_file(exercise_path, exercise_file)
     end
 
     private
--- test/exercise_test.rb
+++ test/exercise_test.rb
@@ -1,4 +1,44 @@
 require "test_helper"
 
 class ExerciseTest < Minitest::Test
+  def test_fetch_exercise
+    user_mock = Minitest::Mock.new
+    user_mock.expect(:account, OpenStruct.new(token: OpenStruct.new(headers: {})))
+
+    Request.stub(:get, nil) do
+      assert_raises APIError do
+        Exercise.fetch(3, 6, 2, user_mock)
+      end
+    end
+
+    user_mock.expect(:account, OpenStruct.new(token: OpenStruct.new(headers: {})))
+
+    Request.stub(:get, {}) do
+      assert_raises APIError do
+        Exercise.fetch(4, 7, 3, user_mock)
+      end
+    end
+
+    user_mock.expect(:account, OpenStruct.new(token: OpenStruct.new(headers: {})))
+
+    Request.stub(:get, { body: { test_path: "a", exercise_path: "b", test_name: "c", exercise_name: "d" }}) do
+      exercise = Exercise.fetch(12, 4, 99, user_mock)
+      assert exercise.test_path == "a"
+      assert exercise.exercise_path == "b"
+      assert exercise.test_name == "c"
+      assert exercise.exercise_name == "d"
+    end
+  end
+
+  def test_test_exercise
+  end
+
+  def test_push_exercise
+    user_mock = Minitest::Mock.new
+    user_mock.expect(:account, OpenStruct.new(token: OpenStruct.new(headers: {})))
+    Request.stub(:post, nil) do
+      exercise = Exercise.new({})
+      exercise.push("", user_mock)
+    end
+  end
 end
